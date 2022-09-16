a06a7b240908e198e2a82a2d5d97910cd5e24aff
Declan <declanisaackuok@gmail.com>
2022-09-16 17:23:57 +0800

    f

--- models/Note.js
+++ models/Note.js
@@ -3,14 +3,16 @@ const noteSchema = new mongoose.Schema({
     title: String,
 
     description:String,
-    user: {type: mongoose.SchemaTypes.ObjectId, ref:"User"}
+    user: {type: mongoose.SchemaTypes.ObjectId, ref:"User"},
+    
+
 
     //otherNote: mongoose.SchemaTypes.ObjectId (if it's for another note)
     //otherStuff: [String]
     //userNotes: userSchema (define the schema)
 
 
-})
+}, {timestamps: true})
 
 /*async function run() {
     await User.create({title: "test", description: "hi"})
--- notes.js
+++ notes.js
@@ -3,30 +3,24 @@ const Note = require("./models/Note")
 const notesRouter = require("express").Router()
 
 notesRouter.route("/").get(async (req, res) => {
-    const notes = await Note.find({user: req.query.id})
+    const notes = await Note.find({user: req.query.id}).sort({"createdAt": -1})
     console.log(notes)
     console.log(req.query.id)
+    console.log("HIHHIHIHIHIHI")
     return res.json(notes)
   
 }).post(async (req, res) => {
     const note = await Note.create({description: req.body.description, title: req.body.title, user: req.body._id})
     console.log(note)
-    return res.status(200).json(req.body)
+    return res.status(200).json(note)
 }).delete(async (req, res) => {
-    console.log(req.body)
-    console.log(req.body)
-    console.log(req.body)
-
-    console.log(req.body)
-    console.log(req.body)
-    console.log(req.body)
-    console.log(req.body)
-    console.log(req.body)
-    console.log(req.body)
-
     const deleted = await Note.findByIdAndDelete(req.body._id)
-    console.log(deleted)
-    return deleted
+    return res.status(200).json(deleted)
+}).patch(async (req, res) => {
+    const updated = await Note.findByIdAndUpdate(req.body._id, {title: req.body.title, description: req.body.description})
+
+    updated.save()
+    return res.status(200).json(updated)
 })
 
 module.exports = notesRouter
